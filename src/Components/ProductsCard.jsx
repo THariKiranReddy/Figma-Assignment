834e1a2a8afd214b88cf32c86e92b249fd7e85ab
Tellapuri Hari Kiran Reddy <harikiranreddytellapuri@gmail.com>
2025-11-21 16:35:48 +0530

    Assignment edited

--- src/Components/ProductsCard.jsx
+++ src/Components/ProductsCard.jsx
@@ -12,7 +12,7 @@ const ProductsCard = () => {
         <SectionTittle className="w-[600px] h-[67px] mx-auto mt-60 relative">Our Top Selling Plants
            <TbBorderCornerRounded className='-mt-20 -ml-4 -rotate-90 text-[#8c8233] text-[80px]'/>
                         <TbBorderCornerRounded className='absolute left-[530px] -bottom-[12px] rotate-90 text-[#8c8233] text-[90px]'/></SectionTittle> 
-        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-x-2  md:gap-x-4  md:gap-20 gap-2 gap-y-40 justify-between mt-40 ml-[47px]' id='buy hidden'>
+        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-x-2  md:gap-x-4  md:gap-20 gap-2 gap-y-40 justify-between mt-40 ml-[47px]' id='buy' >
         {data.map((item,id)=>{
             return <div key={id} className='w-[512px] h-[680px] border-2 rounded-[77px] bg-[#272e22] backdrop-blur-[25px] shadow-[0px_9px_18.4px_0px_#00000040] border-[#646961]'>
                 <img src={item.img} className='w-[459px] h-[459px] object-contain mx-auto -mt-28'/>
